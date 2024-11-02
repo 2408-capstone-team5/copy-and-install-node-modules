515389a1ab1f0a07e2de0d6aba7b5a2ee68a0f08
2408-team5 <2408capstone5@gmail.com>
2024-11-02 11:46:59 -0700

    Update copy_and_install.sh

--- copy_and_install.sh
+++ copy_and_install.sh
@@ -5,26 +5,28 @@ S3_BUCKET="$1"
 S3_DIR="$2"
 TAR_FILE="$3"
 
-# Ensure temp directory exists
-TEMP_DIR="../../../temp"
-mkdir -p $TEMP_DIR
+echo "Hello from inside copy_and_install.sh"
 
-# Copy the node_modules tar file from S3 to temp directory
-echo "Copying $TAR_FILE from S3..."
-sudo su -c "cp ${S3_BUCKET}/${S3_DIR}/${TAR_FILE} ${TEMP_DIR}/" ec2-user
+# # Ensure temp directory exists
+# TEMP_DIR="../../../temp"
+# mkdir -p $TEMP_DIR
 
-# Move the tar file to the working directory
-echo "Moving $TAR_FILE to the working directory..."
-cp "${TEMP_DIR}/${TAR_FILE}" ./
+# # Copy the node_modules tar file from S3 to temp directory
+# echo "Copying $TAR_FILE from S3..."
+# sudo su -c "cp ${S3_BUCKET}/${S3_DIR}/${TAR_FILE} ${TEMP_DIR}/" ec2-user
 
-# Unzip the node_modules tar file
-echo "Unzipping $TAR_FILE..."
-tar -xvzf "$TAR_FILE"
+# # Move the tar file to the working directory
+# echo "Moving $TAR_FILE to the working directory..."
+# cp "${TEMP_DIR}/${TAR_FILE}" ./
 
-# Install dependencies if package.json has changed
-if [[ $(git diff --name-only HEAD~1 HEAD | grep 'package.json') ]]; then
-  echo "Installing dependencies..."
-  npm install
-else
-  echo "No changes in package.json; skipping npm install."
+# # Unzip the node_modules tar file
+# echo "Unzipping $TAR_FILE..."
+# tar -xvzf "$TAR_FILE"
+
+# # Install dependencies if package.json has changed
+# if [[ $(git diff --name-only HEAD~1 HEAD | grep 'package.json') ]]; then
+#   echo "Installing dependencies..."
+#   npm install
+# else
+#   echo "No changes in package.json; skipping npm install."
 fi
